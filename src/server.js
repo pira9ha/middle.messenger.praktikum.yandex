7aa7c7c9cf248da5f4bef315d2eb5786068db367
Saigafarova Aliya <saigafarova00@yandex.ru>
2023-10-29 14:26:14 +0300

    fix: server

--- src/server.js
+++ src/server.js
@@ -1,10 +1,9 @@
 import express from 'express';
-import path from 'path';
 
 const app = express();
 const PORT = 3000;
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static('./dist'));
 
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);

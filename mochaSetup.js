cbf9882333d91953dc135935b66982ea43d56bf5
Saigafarova Aliya <saigafarova00@yandex.ru>
2023-12-21 03:20:06 +0300

    feat: add test for component

--- mochaSetup.js
+++ mochaSetup.js
@@ -1,9 +1,10 @@
 import { JSDOM } from 'jsdom';
 
 const { window } = new JSDOM('<div id="app"></div>', {
-  url: 'http://localhost',
+  url: 'http://localhost:3000',
 });
 
 global.window = window;
 global.document = window.document;
+global.HTMLElement = window.HTMLElement;
 global.DocumentFragment = window.DocumentFragment;
